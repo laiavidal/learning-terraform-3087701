26e7e32fdfa9c21de7eb97e027479b4a4d0d17e6
laiavidal <58323628+laiavidal@users.noreply.github.com>
2023-01-09 01:40:45 +0100

    correction

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -24,7 +24,6 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_dns_hostnames = true
 
   tags = {
     Terraform = "true"
@@ -40,7 +39,7 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.vpc.public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
@@ -56,8 +55,8 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
 
@@ -86,9 +85,9 @@ module "blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  name    = "blog_new"
 
-  vpc_id        = module.vpc.public_subnets[0]
+  name    = "blog"
+  vpc_id        = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
